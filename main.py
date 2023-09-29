6ce5cc59f92754620f5c6a9ca8f31d32c1380162
mirziyod <146424715+00016885@users.noreply.github.com>
2023-09-29 14:34:52 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# value = input("enter a number:")
+# num = int(value)
+# print(num)
+# print(bin(num))
+# print(oct(num))
+# print(hex(num))
+
+# def converter(num, type):
+#     print(num)
+#     print(type(num))
+#
+# converter(5, bin)
