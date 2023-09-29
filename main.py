7e2dc01cb058611a2d55128971dcc371616224e1
mirziyod <146424715+00016885@users.noreply.github.com>
2023-09-29 14:56:18 +0500

    second

--- main.py
+++ main.py
@@ -10,3 +10,25 @@
 #     print(type(num))
 #
 # converter(5, bin)
+
+
+# counter = 0
+# def increment():
+#     global counter
+#     counter += 1
+#
+# increment()
+# print(counter)
+# increment()
+# print(counter)
+#
+#
+def login(username, password):
+    global is_logged_in
+
+    if username == "admin" and password == "password":
+        is_logged_in = True
+    else:
+        is_logged_in = False
+login("admin", "password")
+print(is_logged_in)
